981a091fdaf5eec6d0f7b6f09c332e0bf9e3ff95
Borbás Tibor <47085582+borbastibor@users.noreply.github.com>
2019-12-12 23:41:32 +0100

    update_20191212

--- application/views/partials/menu_view.php
+++ application/views/partials/menu_view.php
@@ -1,5 +1,5 @@
 <div class="navbar-container">
-    <a class="navbar-menuitem" href="home/index">Munkák</a>
+    <a class="navbar-menuitem" href="">Munkák</a>
     <a class="navbar-menuitem" href="#dolgozok">Dolgozók</a>
     <a class="navbar-menuitem" href="#kocsik">Kocsik</a>
 </div>
