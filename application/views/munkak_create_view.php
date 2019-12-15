c8d9c229d30653ad56b8dfb3191888ef83e3d4b9
Borbás Tibor <47085582+borbastibor@users.noreply.github.com>
2019-12-15 16:30:37 +0100

    update_20191215

--- application/views/munkak_create_view.php
+++ application/views/munkak_create_view.php
@@ -1,14 +1,22 @@
 <div class="body-container">
-    <div class="body-title">Új munka létrehozása</div>
+    <div class="body-title">
+        <span>Új munka létrehozása</span>
+        <?php echo(anchor('home','[X]',array('class' => 'cancel-button'))); ?>
+    </div>
     <div class="body-body">
         <form action="" method="POST">
             <label for="jobdate">Dátum:</label><br />
-            <input type="date" id="jobdate"><br /><br />
+            <input type="date" id="jobdate" required><br /><br />
             <label for="jobplace">Helyszín:</label><br />
-            <input type="text" id="jobplace"><br /><br />
+            <input type="text" id="jobplace" required><br /><br />
+            <label for="jobdesc">Leírás:</label><br />
+            <input type="text" id="jobdesc" required><br /><br />
+            <label for="worker">Dolgozó:</label><br />
+            <select id="worker" required></select>
+            <label for="car">Gépjármű:</label><br />
+            <select id="car" required></select>
             <input type="checkbox" name="fixedjob" value="fixed">Átütemezhető?<br /><br />
             <input type="submit" value="Mentés">
-            <div class="cancel-button"><?php echo(anchor('home','Mégse',array('class' => 'cancel-button'))); ?></div>
         </form>
     </div>
 </div>
--- application/views/partials/menu_view.php
+++ application/views/partials/menu_view.php
@@ -1,7 +1,10 @@
+<?php echo(anchor('home/create','<div class="navbar-button">M</div>')); ?>
 <div class="navbar-container">
     <?php
     echo(anchor('home', 'Munkák', array('class' => 'navbar-menuitem')));
+    echo('<br />');
     echo(anchor('dolgozok', 'Dolgozók', array('class' => 'navbar-menuitem')));
+    echo('<br />');
     echo(anchor('kocsik', 'Kocsik', array('class' => 'navbar-menuitem')));
     ?>
 </div>
