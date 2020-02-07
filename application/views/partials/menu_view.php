0b81e2b5cb28da115cc7289e6418fd94be3bbd77
Borbás Tibor <47085582+borbastibor@users.noreply.github.com>
2020-02-07 20:50:53 +0100

    update_20200207

--- application/views/partials/menu_view.php
+++ application/views/partials/menu_view.php
@@ -2,4 +2,6 @@
 echo(anchor("kocsik/index","Gépkocsik",''));
 echo(anchor("dolgozok/index", "Dolgozók",''));
 echo(anchor("munkak/index", "Feladatok",''));
+echo(anchor("gkfutas/index", "Gépkocsifutások",''));
+
 ?>
