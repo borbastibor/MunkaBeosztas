67c627ebe5cc7c57eb3505cc04454c2a9ce614ab
Borbás Tibor <47085582+borbastibor@users.noreply.github.com>
2020-02-19 21:22:52 +0100

    fix_calendarview

--- application/views/partials/menu_view.php
+++ application/views/partials/menu_view.php
@@ -1,11 +1,23 @@
-<?php
-echo(anchor("kocsik/index","Gépkocsik",''));
-echo(anchor("dolgozok/index", "Dolgozók",''));
-echo(anchor("munkak/index", "Feladatok",''));
-echo(anchor("gkfutas/index", "Gépkocsifutások",''));
-echo(anchor("home/shiftOneDayBack", "<",''));
-echo(anchor("home/shiftOneDayFwd", ">",''));
-echo(anchor("home/resetToCurrentDay", "C",''));
-echo(anchor("home/shiftOneWeekBack", "<<",''));
-echo(anchor("home/shiftOneWeekFwd", ">>",''));
-?>
+<div class="dropdown">
+    <button class="dropbtn">Controls</button>
+    <div class="dropdown-content">
+        <?php
+        echo(anchor('home/shiftOneDayBack', 'Vissza 1 napot','class="dropdown-content"'));
+        echo(anchor('home/shiftOneDayFwd', 'Előre 1 napot','class="dropdown-content"'));
+        echo(anchor('home/shiftOneWeekBack', 'Vissza 7 napot','class="dropdown-content"'));
+        echo(anchor('home/shiftOneWeekFwd', 'Előre 7 napot','class="dropdown-content"'));
+        echo(anchor('home/resetToCurrentDay', 'Aktuális nap','class="dropdown-content"'));
+        ?>
+    </div>
+</div>
+<div class="dropdown">
+    <button class="dropbtn">Admin</button>
+    <div class="dropdown-content">
+        <?php
+        echo(anchor('kocsik/index','Gépkocsik','class="dropdown-content"'));
+        echo(anchor('dolgozok/index', 'Dolgozók','class="dropdown-content"'));
+        echo(anchor('munkak/index', 'Feladatok','class="dropdown-content"'));
+        echo(anchor('gkfutas/index', 'Gépkocsifutások','class="dropdown-content"'));
+        ?>
+    </div>
+</div>
