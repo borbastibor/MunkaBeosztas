cfa107625ce42c1bcddfb39292236409d168bb90
Borbás Tibor <47085582+borbastibor@users.noreply.github.com>
2020-02-21 20:43:51 +0100

    fix_menuview

--- application/views/partials/menu_view.php
+++ application/views/partials/menu_view.php
@@ -1,11 +1,13 @@
-<div class="dropdown">
-    <button class="dropbtn">Admin</button>
-    <div class="dropdown-content">
-        <?php
-        echo(anchor('kocsik/index', 'Gépkocsik', ''));
-        echo(anchor('dolgozok/index', 'Dolgozók', ''));
-        echo(anchor('munkak/index', 'Feladatok', ''));
-        echo(anchor('gkfutas/index', 'Gépkocsifutások', ''));
-        ?>
+<div class="dropbtn-container">
+    <div class="dropdown">
+        <button class="dropbtn">Admin</button>
+        <div class="dropdown-content">
+            <?php
+            echo(anchor('kocsik/index', 'Gépkocsik', ''));
+            echo(anchor('dolgozok/index', 'Dolgozók', ''));
+            echo(anchor('munkak/index', 'Feladatok', ''));
+            echo(anchor('gkfutas/index', 'Gépkocsifutások', ''));
+            ?>
+        </div>
     </div>
 </div>
