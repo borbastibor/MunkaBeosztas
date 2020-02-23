c37907a3f855877668bd10231ef520a4705fc5ff
Borbás Tibor <47085582+borbastibor@users.noreply.github.com>
2020-02-23 14:45:54 +0100

    fix_navigation menu

--- application/views/partials/menu_view.php
+++ application/views/partials/menu_view.php
@@ -1,31 +1,17 @@
 <?php
-    echo('<table>');
-    echo('<tr>');
-    echo('<td>');
+    echo('<div class="menu-control-container">');
     if ($iscalendarview) {
-        echo('<div class="menu-control-container">');
         echo(anchor('home/shiftOneDayBack', '<', array('class' => 'calendar-control-btn')));
         echo(anchor('home/shiftOneWeekBack', '&#8810', array('class' => 'calendar-control-btn')));
         echo(anchor('home/resetToCurrentDay', 'C', array('class' => 'calendar-control-btn')));
         echo(anchor('home/shiftOneWeekFwd', '&#8811', array('class' => 'calendar-control-btn')));
         echo(anchor('home/shiftOneDayFwd', '>', array('class' => 'calendar-control-btn')));
-        echo('</div>');
     }
-    echo('</td>');
-
-    echo('<td>');
     if ($isadmin != null) {
-        echo('<div class="dropdown">');
-        echo('<button class="dropbtn">Admin</button>');
-        echo('<div class="dropdown-content">');
-        echo(anchor('kocsik/index', 'Gépkocsik', ''));
-        echo(anchor('dolgozok/index', 'Dolgozók', ''));
-        echo(anchor('munkak/index', 'Feladatok', ''));
-        echo(anchor('gkfutas/index', 'Gépkocsifutások', ''));
-        echo('</div>');
-        echo('</div>');
+        echo(anchor('kocsik/index', 'Gépkocsik', array('class' => 'calendar-control-btn')));
+        echo(anchor('dolgozok/index', 'Dolgozók', array('class' => 'calendar-control-btn')));
+        echo(anchor('munkak/index', 'Feladatok', array('class' => 'calendar-control-btn')));
+        echo(anchor('gkfutas/index', 'Gépkocsifutások', array('class' => 'calendar-control-btn')));
     }
-    echo('</td>');
-    echo('</tr>');
-    echo('</table>');
+    echo('</div>');
 ?>
